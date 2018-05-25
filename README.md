f3748fee3b6379ba75d22c5bdb6d1121c4265aad
Sylvain Lesage <severo@rednegra.net>
2018-05-25 12:30:46 -0400

    Script funcional

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ Luego llamar el script con:
 ```bash
 python get_list_statistics.py
 ```
+
+El resultado se encuentra en el archivo especificado en `config.yml`, por ejemplo, si `config.yml` contiene:
+
+```yaml
+export:
+  filepath: /tmp/
+  filename_base: listas
+```
+
+el archivo creado el 25/05/2018 a las 12:29:35 será `/tmp/listas_20180525-122935.csv`.
--- get_list_statistics.py
+++ get_list_statistics.py
@@ -1,10 +1,38 @@
-import yaml
+import os
+import time
 import twitter
+import yaml
+
 
 with open("config.yml", 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
+
+# Creación del archivo CSV de exportación
+timestr = time.strftime("%Y%m%d-%H%M%S")
+csvFileName = os.path.join(cfg["export"]["filepath"], cfg["export"]["filename_base"] + "_" + timestr + ".csv")
 
 api = twitter.Api(consumer_key=cfg['twitter']['consumer_key'],
                   consumer_secret=cfg['twitter']['consumer_secret'],
                   access_token_key=cfg['twitter']['access_token_key'],
                   access_token_secret=cfg['twitter']['access_token_secret'])
+
+headerStr = "usuario_lista,lista,usuario,nombre,seguidores,siguiendo,tweets,ultimo_tweet"
+with open(csvFileName,'wt') as file:
+    file.write(headerStr)
+    file.write('\n')
+    for list in cfg["lists"]:
+        results = api.GetListMembers(slug=list["list"], owner_screen_name=list["user"], skip_status=False, include_entities=False)
+        for r in results:
+            rowStr = "\"" + str(list["user"]) + "\""
+            rowStr += "," + "\"" + str(list["list"]) + "\""
+            rowStr += "," + "\"" + str(r.screen_name) + "\""
+            rowStr += "," + "\"" + str(r.name) + "\""
+            rowStr += "," + "\"" + str(r.followers_count) + "\""
+            rowStr += "," + "\"" + str(r.friends_count) + "\""
+            rowStr += "," + "\"" + str(r.statuses_count) + "\""
+            if r.status:
+                rowStr += "," + "\"" + time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(r.status.created_at,"%a %b %d %H:%M:%S +0000 %Y")) + "\""
+            else:
+                rowStr += "," + "\"\""
+            file.write(rowStr)
+            file.write('\n')
